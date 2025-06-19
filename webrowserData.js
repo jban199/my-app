7efdb5aa21c99481d102d055fda7391926f6239a
jban199 <jeetbanerjee.ban@gmail.com>
2025-06-19 20:05:56 +0530

    Add files via upload

---
+++ webrowserData.js
@@ -0,0 +1,310 @@
+// webrowserData.js
+window.ts_webData = [
+	{
+		title: "Google",
+		description: "Search the world's information, including webpages, images, videos and more. Google has many special features to help you find exactly what you're looking",
+		link: "https://www.google.com",
+		image: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+		tags: "Search Engine"
+	},
+	{
+		title: "ChatGPT",
+		description: "",
+		link: "https://chat.openai.com",
+		image: "https://chat.openai.com/apple-touch-icon.png",
+		tags: "AI, Productivity"
+	},
+	{
+		title: "Wikipedia",
+		description: "",
+		link: "https://www.wikipedia.org",
+		image: "https://www.wikipedia.org/static/apple-touch/wikipedia.png",
+		tags: "Education, Reference"
+	},
+	{
+		title: "Cricbuzz",
+		description: "",
+		link: "https://www.cricbuzz.com",
+		image: "https://www.cricbuzz.com/images/cb_logo.svg",
+		tags: "Sports, Cricket"
+	},
+	{
+		title : "GitHub",
+		description: "",
+		link : "https://github.com",
+		image : "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+		tags : ""
+	},
+	{
+		title: "Amazon India",
+		description: "",
+		link: "https://www.amazon.in",
+		image: "https://www.amazon.in/favicon.ico",
+		tags: "E-commerce, Shopping"
+	},
+	{
+		title: "Hindustan Times",
+		description: "",
+		link: "https://www.hindustantimes.com",
+		image: "https://www.hindustantimes.com/res/images/ht-logo.svg",
+		tags: "News"
+	},
+	{
+		title: "YouTube",
+		description: "",
+		link: "https://www.youtube.com",
+		image: "https://www.youtube.com/s/desktop/6b4e0b8b/img/favicon_144x144.png",
+		tags: "Video, Entertainment"
+	},
+	{
+		title: "Bing",
+		description: "",
+		link: "https://www.bing.com",
+		image: "https://www.bing.com/sa/simg/favicon-2x.ico",
+		tags: "Search Engine"
+	},
+	{
+		title: "Reddit",
+		description: "",
+		link: "https://www.reddit.com",
+		image: "https://www.redditstatic.com/desktop2x/img/favicon/apple-icon-180x180.png",
+		tags: "Forums, Social Media"
+	},
+	{
+		title: "LinkedIn",
+		description: "",
+		link: "https://www.linkedin.com",
+		image: "https://static.licdn.com/scds/common/u/images/logos/favicons/v1/favicon.ico",
+		tags: "Professional, Networking"
+	},
+	{
+		title: "ESPNcricinfo",
+		description: "",
+		link: "https://www.espncricinfo.com",
+		image: "https://www.espncricinfo.com/apple-touch-icon.png",
+		tags: "Sports, Cricket"
+	},
+	{
+		title: "Instagram",
+		description: "",
+		link: "https://www.instagram.com",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Social Media, Photo Sharing"
+	},
+	{
+		title: "Facebook",
+		description: "",
+		link: "https://www.facebook.com",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Social Media"
+	},
+	{
+		title: "WhatsApp Web",
+		description: "",
+		link: "https://web.whatsapp.com/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Messaging, Communication"
+	},
+	{
+		title: "X (formerly Twitter)",
+		description: "",
+		link: "https://x.com",
+		image: "https://abs.twimg.com/responsive-web/client-web/icon-ios.77d25eba.png",
+		tags: "Social Media, News"
+	},
+	{
+		title: "Times of India",
+		description: "",
+		link: "https://timesofindia.com",
+		image: "https://timesofindia.indiatimes.com/icons/toifavicon.ico",
+		tags: "News, Media"
+	},
+	{
+		title: "Flipkart",
+		description: "",
+		link: "https://www.flipkart.com",
+		image: "https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/logo_lite-cbb357.png",
+		tags: "Shopping, E-commerce"
+	},
+	{
+		title: "Hotstar",
+		description: "",
+		link: "https://www.hotstar.com",
+		image: "https://secure-media.hotstar.com/web-assets/prod/jhs_favicon.ico",
+		tags: "Streaming, Entertainment"
+	},
+	{
+		title: "Quora",
+		description: "",
+		link: "https://www.quora.com",
+		image: "https://qsf.cf2.quoracdn.net/-4-images.favicon-new.ico-26-07ecf7cd341b6919.ico",
+		tags: "Q&A, Knowledge"
+	},
+	{
+		title: "MSN",
+		description: "",
+		link: "https://www.msn.com",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "News"
+	},
+	{
+		title: "Drik Panchang",
+		description: "",
+		link: "https://www.drikpanchang.com/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Astrology"
+	},
+	{
+		title: "Temp Mail",
+		description: "",
+		link: "https://temp-mail.org/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Tools"
+	},
+	{
+		title: "Diffchecker",
+		link: "https://www.diffchecker.com/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Tools"
+	},
+	{
+		title: "Perchance",
+		description: "",
+		link: "https://perchance.org/ai-text-to-image-generator",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "AI"
+	},
+	{
+		title: "Airtel IN",
+		description: "",
+		link: "https://www.airtel.in/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Recharge"
+	},
+	{
+		title: "Alliance Broadband Payment",
+		description: "",
+		link: "https://pay2abs.alliancebroadband.in/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Recharge"
+	},
+	{
+		title: "VI - Vodafone Idea",
+		description: "",
+		link: "https://www.myvi.in/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Recharge"
+	},
+	{
+		title: "WB Health Online OPD",
+		description: "",
+		link: "https://onlinehmis.wbhealth.gov.in/Login.aspx",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Medical"
+	},
+	{
+		title: "SpeedoMeter",
+		description: "",
+		link: "https://speedometer.live/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Tools"
+	},
+	{
+		title: "DuckDuckGo",
+		description: "",
+		link: "https://duckduckgo.com/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Search Engine"
+	},
+	{
+		title: "Speedtest by Ookla",
+		description: "",
+		link: "https://www.speedtest.net/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Tools"
+	},
+	{
+		title: "NDTV Live TV",
+		description: "",
+		link: "https://www.ndtv.com/livetv-ndtv24x7",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "News"
+	},
+	{
+		title: "Amazon MX Player",
+		description: "",
+		link: "https://www.mxplayer.in/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Entertainment"
+	},
+	{
+		title: "ZEE5",
+		description: "",
+		link: "https://www.zee5.com/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Entertainment"
+	},
+	{
+		title: "Compress JPEG Images Online",
+		description: "",
+		link: "https://compressjpeg.com/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Tools"
+	},
+	{
+		title: "Online MP3 Cutter",
+		description: "",
+		link: "https://mp3cut.net/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Tools"
+	},
+	{
+		title: "Online Audio Joiner",
+		description: "",
+		link: "https://audio-joiner.com/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Tools"
+	},
+	{
+		title: "Hearing Test",
+		description: "",
+		link: "https://www.echalk.co.uk/Science/biology/hearing/HowOldIsYourHearing/resource.html",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Health"
+	},
+	{
+		title: "A Soft Murmur",
+		description: "",
+		link: "https://asoftmurmur.com/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Tools"
+	},
+	{
+		title: "myNoise",
+		description: "",
+		link: "https://mynoise.net/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Tools"
+	},
+	{
+		title: "Defonic",
+		description: "",
+		link: "https://defonic.com/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Tools"
+	},
+	{
+		title: "ElevenLabs",
+		description: "",
+		link: "https://elevenlabs.io/",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "Tools"
+	},
+	{
+		title: "XXXX",
+		description: "",
+		link: "XXXX",
+		image: "images/nm/nm-placeholder.svg",
+		tags: "XXXX"
+	}
+];
